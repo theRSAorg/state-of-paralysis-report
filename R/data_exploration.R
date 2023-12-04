d35b4680a0f22a1826a0fdce6870c5b3ce63ba16
Jolyon Miles-Wilson <Jolyon.Miles-Wilson@rsa.org.uk>
2023-12-04 16:33:08 +0000

    add data exploration script for playing around

---
+++ R/data_exploration.R
@@ -0,0 +1,61 @@
+rm(list = ls()) # clear the workspace
+# Packages
+packages <- c('haven','dplyr','ggplot2','forcats')
+pkg_notinstall <- packages[!(packages %in% installed.packages()[,"Package"])]
+lapply(pkg_notinstall, install.packages, dependencies = TRUE)
+lapply(packages, library, character.only = TRUE)
+
+rsa_palette <- c("#03ECDD",
+                 "#000000",
+                 "#FFFFFF",
+                 "#000C78",
+                 "#FF21B8",
+                 "#FFA72F",
+                 "#FF2800")
+
+rsa_extra_palette <- c("#F5F5F5",
+                    "#373737",
+                    "#21DCFF")
+
+data <- haven::read_sav("./data/househol.sav")
+
+dplyr::glimpse(data)
+
+# Summarise tenure by age group
+tenure_tab <- data %>%
+  mutate(
+    # apply labels and create better names
+    age = haven::as_factor(HHAGEGR3),
+    tenure = as_factor(PTENTYP2)
+  ) %>%
+  mutate(
+    # collapse tenure to match original doc
+    tenure = forcats::fct_collapse(tenure,
+                                   "Mortgage" = "Owned with mortgage",
+                                   "Own outright" = "Owned outright",
+                                   "Private rent" = c("Rented privately unfurnished",
+                                                      "Rented privately furnished"),
+                                   "Social rent" = c("Rented from Council",
+                                                     "Rented from Housing Association")),
+    # match level order to original doc
+    tenure = factor(tenure, 
+                      levels =  c("Mortgage",
+                      "Own outright",
+                      "Private rent",
+                      "Social rent")
+      )
+    ) %>%
+  # summarise etc
+  group_by(age, tenure) %>%
+  summarise(
+    n = n()
+  ) %>%
+  mutate(
+    percentage = 100 * (n / sum(n))
+  )
+
+# plot
+ggplot2::ggplot(tenure_tab, aes(age, percentage, fill = tenure, group = tenure)) +
+  geom_col(position = "dodge", colour = "black") +
+  scale_fill_manual(values = rsa_palette)
+
