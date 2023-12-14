853a1f47a98fc1ee5db6e490c80b7d079846ff6b
Eirini Zormpa <eirini.zormpa@rsa.org.uk>
2023-12-14 11:58:38 +0000

    add code for fig 1.6

--- R/data_exploration.R
+++ R/data_exploration.R
@@ -186,3 +186,66 @@ figure1_5 <- tenure_data_factors %>%
 # ggsave(filename = "./figures/figure1_4_housing_tenure_2021-22.png")
 # ggsave(filename = "./figures/figure1_5_housing_tenure.png", width = 9, height = 3.62)
 
+# fig 1.6
+frs_2021_2022 <- read_tsv(here("data", "frs-survey", "2008-2022", "2021-2022_househol.tab"))
+
+fig_1.6_data <- frs_2021_2022 %>%
+  select(id = SERNUM,
+         age = HHAGEGR3,
+         income_num = HHINC,
+         income_band = HHINCBND,
+         housing_costs_gb = GBHSCOST,
+         housing_costs_ni = NIHSCOST,
+         tenure = PTENTYP2) %>%
+  mutate(
+    age = fct_recode(as_factor(age),
+                     "16-24" = "1",
+                     "25-34" = "2",
+                     "35-44" = "3",
+                     "45-54" = "4",
+                     "55-59" = "5",
+                     "60-64" = "6",
+                     "65-74" = "7",
+                     "75+" = "8"),
+    income_band = fct_recode(as_factor(income_band),
+                             "Under £200 a week" = "1",
+                             "£200 and less than £400" = "2",
+                             "£400 and less than £600" = "3",
+                             "£600 and less than £800" = "4",
+                             "£800 and less than £1000" = "5",
+                             "£1000 and less than £1200" = "6",
+                             "£1200 and less than £1400" = "7",
+                             "£1400 and less than £1600" = "8",
+                             "£1600 and less than £1800" = "9",
+                             "£1800 and less than £2000" = "10",
+                             "Above £2000" = "11"),
+    housing_costs_gb = na_if(housing_costs_gb, -1),
+    housing_costs_ni = na_if(housing_costs_ni, -1),
+    tenure = fct_recode(as_factor(tenure),
+                        "Rented from Council" = "1",
+                        "Rented from Housing Association" = "2",
+                        "Rented privately unfurnished" = "3",
+                        "Rented privately furnished" = "4",
+                        "Owned outright" = "5" ,
+                        "Owned with mortgage" = "6")) %>% 
+  unite("housing_costs", housing_costs_gb:housing_costs_ni, na.rm = TRUE) %>% 
+  mutate(housing_costs = as.numeric(housing_costs),
+         percentage = (housing_costs/income_num)*100)
+
+# find a definition of affordability and show how many people do or do not have it by age group
+
+fig1.6 <- fig_1.6_data %>% 
+  group_by(age) %>% 
+  summarise(median_percentage = median(percentage, na.rm = TRUE)) %>% 
+  ggplot(aes(x = age, y = median_percentage)) +
+  geom_col(fill = "#000C78") +
+  scale_y_continuous(n.breaks = 10) +
+  labs(x = "Age",
+       y = "Median percentage") +
+  theme_classic()
+
+glimpse(fig_1.6_data)
+levels(as_factor(frs_2020_2022$HHAGEGR3))
+levels(as_factor(fig_1.6_data$income_band))
+
+# ggsave(filename = "./figures/figure1_6_housing-costs-income-percentage.png", width = 9, height = 3.62)
